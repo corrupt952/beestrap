e90c1197179883fadf0b989b8fc1df0af923d39c
K@zuki. <k@zuki.dev>
2023-11-22 15:55:25 +0900

    change: rebind address of netdata

--- recipes/common.rb
+++ recipes/common.rb
@@ -11,6 +11,14 @@ package 'netdata' do
   user 'root'
   action :install
 end
+execute "change netadata binding address" do
+  user "root"
+  command "sed -i -e 's/bind socket to IP = 127.0.0.1/bind socket to IP = 0.0.0.0/g' /etc/netdata/netdata.conf"
+end
+service 'netdata' do
+  user 'root'
+  action :restart
+end
 
 # プロセスを可視化するツール
 # https://htop.dev/
